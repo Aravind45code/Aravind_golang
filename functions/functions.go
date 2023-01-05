f92b5ac9d67e3ad727e7a80312cf14efba040cfa
aravindreddy <aravindreddyveerelli@gmail.com>
2023-01-05 11:21:11 +0530

    first commit

---
+++ functions/functions.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+func double(x int) int{
+	return x+x
+}
+func add(lhs,rhs int) int{
+	return lhs+rhs
+}
+func greet(){
+	fmt.Println("hello from greeting function")
+}
+func returnTwoNumbers() (int ,int){
+	return 2,2
+}
+func five() int{
+	return 5
+}
+func addThree(a,b,c int) int{
+	return a+b+c
+}
+func greetPerson(name string){
+	fmt.Println("hello",name)
+
+}
+func hiThere() string{
+	return "hithere"
+}
+func main() {
+	greet()
+   dozen:=double(6)
+   fmt.Println("dozen is",dozen)
+   dozen2:=add(dozen,1)
+   fmt.Println("the dozen2 is",dozen2)
+   dozen3:=add(double(dozen2),1);
+   fmt.Println("the dozen3 is",dozen3)
+   a,b:=returnTwoNumbers()
+   fmt.Println("the returned numbers are ",a,b)
+
+   fmt.Println(five())
+   afteraddition:=addThree(1,2,3)
+   fmt.Println("The three number sum is",afteraddition)
+   greetPerson("Aravind")
+   fmt.Println(hiThere())
+
+
+}
---
+++ variables/variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+	 var myName="Aravind"
+	 fmt.Println("my name is",myName,myName)
+	 var name string="Reddy"
+	 fmt.Println("name= ",name)
+	 userName:="Rohit"
+	 fmt.Println("Hello",userName)
+	 var sum int
+	 fmt.Println("The sum is",sum)
+	 part1,other:=1,5
+	 fmt.Println("the part1 is",part1," and the other one is",other)
+	 part2,other:=9,0
+	 fmt.Println("the part2 is ",part2,"the other one is",other)
+	 part2=56
+	 fmt.Println("the changed part2 is",part2)
+	 sum=part1+part2
+	 fmt.Println("the sum after addition is",sum)
+	 var(
+		 lessonName="variables"
+		 lessonPeroid="6months"
+	 )
+	 fmt.Println("the lesson and lessonperoid are",lessonName,"and",lessonPeroid)
+
+
+
+ }
